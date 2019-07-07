dab1a0982bac267a91aeaa016b3daa486985829a
martinyounghoonkim <hoons0131@gmail.com>
2019-07-07 16:18:48 +0900

    Add example action's test case describe

--- frotend/src/stores/actions/example/exampleAction.__tests__.ts
+++ frotend/src/stores/actions/example/exampleAction.__tests__.ts
@@ -7,18 +7,18 @@ import {
   fetchExampleSuccessAction
 } from "./exampleAction";
 
-describe("Abount Example's action creators", () => {
-  it("", () => {
+describe("예제 Action creator 함수에 대해서", () => {
+  it("API를 패치하는 액션 함수를 실행 시, API 요청에 해당하는 객체를 반환한다.", () => {
     expect(fetchExampleRequestAction()).toEqual({
       type: FETCH_EXAMPLE_REQUEST,
     });
   });
-  it("", () => {
+  it("API를 패치 성공 액션 함수를 실행 시, API 요청 성공에 해당하는 객체를 반환한다.", () => {
     expect(fetchExampleSuccessAction()).toEqual({
       type: FETCH_EXAMPLE_SUCCESS,
     });
   });
-  it("", () => {
+  it("API를 패치 실패 액션 함수를 실행 시, API 요청 실패에 해당하는 객체를 반환한다.", () => {
     const message = "Something's wrong in API.";
     expect(fetchExampleFailureAction(message)).toEqual({
       type: FETCH_EXAMPLE_FAILURE, message
