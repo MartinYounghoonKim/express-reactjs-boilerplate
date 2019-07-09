146e165a700aaa35eb05d6d43741ac06166575d2
martinyounghoonkim <hoons0131@gmail.com>
2019-07-09 23:44:08 +0900

    Add sequalize with sqlite3

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../src/models/index").User;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({"test": 1});
+router.get('/', async function (req, res, next) {
+  try {
+    const users = await User.findAll();
+    console.log(users);
+    // await User.create({
+    //   firstName: "aaa",
+    //   lastName: "bbb",
+    //   email: "ccc",
+    // });
+  } catch (e) {
+    console.log("error =>", e);
+  }
+  res.send({ "test": "1" });
 });
 router.get('/test', function(req, res, next) {
   res.status(500).json({ message: "테스트" });
